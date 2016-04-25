41ccae77ec0c58a2d59faff51a13319703f35dd7
renanarceno <r3n44n@gmail.com>
2016-04-25 17:29:35 -0300

    impedir que jogue em um lugar já jogado

--- Gomoku/src/Board.java
+++ Gomoku/src/Board.java
@@ -8,9 +8,11 @@ import java.util.Set;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 import org.jgrapht.EdgeFactory;
 import org.jgrapht.UndirectedGraph;
+
 /*
  * H = jogada sendo avaliada
  * X = jogada comp
@@ -22,27 +24,27 @@ import org.jgrapht.UndirectedGraph;
  * COMP		2^0		2^1		2^2		2^3		2^4		2^5		2^6		2^7		2^8		2^9
  */
 public class Board extends JFrame implements ActionListener {
-	
+
 	public static final int TAMANHO = 15;
 	public static final int ESPACAMENTO = 1;
 
 	private static final long serialVersionUID = 1L;
-	
+
 	public Tile[][] board;
-	
+
 	SnapShoot g;
 	Tile nullTile;
-	
+
 	int gHeuristic;
 	int computerGHeuristic;
 	int humanGHeuristic;
-	
+
 	public Board() {
 		Container cp = this.getContentPane();
 		cp.setLayout(new GridLayout(TAMANHO, TAMANHO, ESPACAMENTO, ESPACAMENTO));
 		cp.setBackground(Color.GREEN);
-		for(int i = 1; i <= TAMANHO; i++)
-			for(int j = 1; j <= TAMANHO; j++) {
+		for (int i = 1; i <= TAMANHO; i++)
+			for (int j = 1; j <= TAMANHO; j++) {
 				String nome = i + "~" + j;
 				JButton labelNome = new JButton();
 				labelNome.setName(nome);
@@ -51,22 +53,21 @@ public class Board extends JFrame implements ActionListener {
 				labelNome.setVisible(true);
 				cp.add(nome, labelNome);
 			}
-		
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setTitle("Gomoku");  
+		setTitle("Gomoku");
 		setSize(496, 489);
 		setVisible(true);
 		board = new Tile[17][17];
-		for(int i = 0; i < 17; i++)
-			for(int j = 0; j < 17; j++)
-				if((i == 0 || j == 0) || (i == 16 || j == 16))
+		for (int i = 0; i < 17; i++)
+			for (int j = 0; j < 17; j++)
+				if ((i == 0 || j == 0) || (i == 16 || j == 16))
 					board[i][j] = new Tile(i, j, Piece.UNAVALIABLE);
 				else
 					board[i][j] = new Tile(i, j, Piece.EMPTY);
-					
-		
+
 	}
-	
+
 	public static void main(String... args) {
 		new Board().snapShoot();
 	}
@@ -74,53 +75,53 @@ public class Board extends JFrame implements ActionListener {
 	private void snapShoot() {
 		// TODO Auto-generated method stub
 		g = new SnapShoot(Link.class);
-		nullTile = new Tile(-1 , -1, null);
+		nullTile = new Tile(-1, -1, null);
 		g.addVertex(nullTile);
 	}
 
 	private boolean vez = true;
-	
+
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
-		JButton peca = (JButton)arg0.getSource();
+		JButton peca = (JButton) arg0.getSource();
+		if (peca.getBackground() == Color.WHITE || peca.getBackground() == Color.BLACK)
+			return;
 		String[] position = peca.getName().split("~");
 		int linha = Integer.valueOf(position[0]);
 		int coluna = Integer.valueOf(position[1]);
-		
-		if(peca.getBackground() == Color.GRAY) {
+
+		if (peca.getBackground() == Color.GRAY) {
 			jogar(linha, coluna, false);
 		}
-		//vez = !vez;
+		// vez = !vez;
 		MinMax m = new MinMax(this);
 		int[] jogada = m.minMax(true, 3, Integer.MIN_VALUE, Integer.MAX_VALUE);
 		jogar(jogada[1], jogada[2], true);
 
 	}
-	
+
 	public void jogar(int linha, int coluna, boolean player) {
-		JButton peca = (JButton)this.getContentPane().getComponent((linha * 15) - (16 - coluna));
-		
+		JButton peca = (JButton) this.getContentPane().getComponent((linha * 15) - (16 - coluna));
+
 		Piece piece;
-		
-		if(player) {
+
+		if (player) {
 			peca.setBackground(Color.BLACK);
 			piece = Piece.BLACK;
-		}
-		else {
+		} else {
 			peca.setBackground(Color.WHITE);
 			piece = Piece.WHITE;
 		}
-		
+
 		board[linha][coluna].setPiece(piece);
-		
+
 		Tile evaluating = board[linha][coluna];
 		g.addVertex(evaluating);
-		
-		g.addEdge(evaluating, nullTile , new Link(null, HeuristicUtil.bitPower[4]));
-		
+
+		g.addEdge(evaluating, nullTile, new Link(null, HeuristicUtil.bitPower[4]));
+
 		Tile iterating;
-		
+
 		boolean goRight = true;
 		boolean goLeft = true;
 		boolean goUp = true;
@@ -129,44 +130,43 @@ public class Board extends JFrame implements ActionListener {
 		boolean goAscendantLeft = true;
 		boolean goDescendantRight = true;
 		boolean goDescendantLeft = true;
-		
-		for(int distance = 1; distance < 5; distance++) {
-			if(goRight) {
+
+		for (int distance = 1; distance < 5; distance++) {
+			if (goRight) {
 				iterating = board[linha + distance][coluna];
 				goRight = makeEdge(evaluating, iterating, Orientation.LINE, distance);
 			}
-			if(goLeft) {
+			if (goLeft) {
 				iterating = board[linha - distance][coluna];
 				goLeft = makeEdge(evaluating, iterating, Orientation.LINE, -distance);
 			}
-			if(goUp) {
+			if (goUp) {
 				iterating = board[linha][coluna + distance];
 				goUp = makeEdge(evaluating, iterating, Orientation.COLUMN, distance);
 			}
-			if(goDown) {
+			if (goDown) {
 				iterating = board[linha][coluna - distance];
 				goDown = makeEdge(evaluating, iterating, Orientation.COLUMN, -distance);
 			}
-			if(goAscendantRight) {
+			if (goAscendantRight) {
 				iterating = board[linha + distance][coluna + distance];
 				goAscendantRight = makeEdge(evaluating, iterating, Orientation.ASCENDANT_DIAGONAL, distance);
 			}
-			if(goAscendantLeft) {
+			if (goAscendantLeft) {
 				iterating = board[linha - distance][coluna - distance];
 				goAscendantLeft = makeEdge(evaluating, iterating, Orientation.ASCENDANT_DIAGONAL, -distance);
 			}
-			if(goDescendantRight) {
+			if (goDescendantRight) {
 				iterating = board[linha + distance][coluna - distance];
 				goDescendantRight = makeEdge(evaluating, iterating, Orientation.DESCENDANT_DIAGONAL, -distance);
 			}
-			if(goDescendantLeft) {
+			if (goDescendantLeft) {
 				iterating = board[linha - distance][coluna + distance];
 				goDescendantLeft = makeEdge(evaluating, iterating, Orientation.DESCENDANT_DIAGONAL, distance);
 			}
 		}
-		
 
-		if(player) {
+		if (player) {
 			computerGHeuristic += heuristic(evaluating);
 		} else {
 			humanGHeuristic += heuristic(evaluating);
@@ -175,46 +175,45 @@ public class Board extends JFrame implements ActionListener {
 	}
 
 	private boolean makeEdge(Tile evaluating, Tile iterating, Orientation orientation, int relativePosition) {
-		if(iterating.getPiece().equals(Piece.UNAVALIABLE)) {
+		if (iterating.getPiece().equals(Piece.UNAVALIABLE)) {
 			g.addEdge(evaluating, nullTile, new Link(orientation, HeuristicUtil.bitPower[13 + relativePosition]));
 			return false;
 		}
-		if(iterating.getPiece().equals(Piece.EMPTY))
+		if (iterating.getPiece().equals(Piece.EMPTY))
 			return true;
-		if(evaluating.getPiece().equals(iterating.getPiece())) {
+		if (evaluating.getPiece().equals(iterating.getPiece())) {
 			g.addEdge(evaluating, iterating, new Link(orientation, HeuristicUtil.bitPower[4 + relativePosition]));
 			return true;
-		}
-		else
+		} else
 			g.addEdge(evaluating, iterating, new Link(orientation, HeuristicUtil.bitPower[13 + relativePosition]));
 		return false;
 	}
-	
+
 	public int heuristic(Tile evaluating) {
 		int heuristicValue = 0;
-		for(Orientation orientation : Orientation.values()) {
+		for (Orientation orientation : Orientation.values()) {
 			int sumPositions = 0;
-			for(Link edge : g.getEdgesWith(evaluating, orientation))
+			for (Link edge : g.getEdgesWith(evaluating, orientation))
 				sumPositions += edge.weigth;
 			heuristicValue += heuristicIt(sumPositions);
-			
-			//System.out.println(sumPositions + "       " + heuristicValue);
+
+			// System.out.println(sumPositions + " " + heuristicValue);
 		}
 		return heuristicValue;
 	}
-	
+
 	private int heuristicIt(int positions) {
 		int evaluating = 0;
 		int maskRedundancy = 0;
-		for(Mask mask : Mask.values()) {
-			for(int i = mask.spatialRedundancy; i >= 0 ; i--) {
+		for (Mask mask : Mask.values()) {
+			for (int i = mask.spatialRedundancy; i >= 0; i--) {
 				maskRedundancy = mask.mask << i;
 				evaluating = positions & maskRedundancy;
 				evaluating = evaluating >> i;
-				if(mask.equals(Mask.XXXXX) && evaluating == mask.value) {
-					System.out.println("venceu");
+				if (mask.equals(Mask.XXXXX) && evaluating == mask.value) {
+					System.out.println("venceu ou quase venceu");
 				}
-				if(evaluating == mask.value)
+				if (evaluating == mask.value)
 					return mask.heuristic;
 			}
 		}
@@ -227,10 +226,10 @@ public class Board extends JFrame implements ActionListener {
 	}
 
 	public void desjogar(int line, int column) {
-		JButton peca = (JButton)this.getContentPane().getComponent((line * 15) - (16 - column));
+		JButton peca = (JButton) this.getContentPane().getComponent((line * 15) - (16 - column));
 		peca.setBackground(Color.GRAY);
 		Tile evaluating = board[line][column];
-		if(evaluating.getPiece().equals(Piece.BLACK)) {
+		if (evaluating.getPiece().equals(Piece.BLACK)) {
 			computerGHeuristic -= heuristic(evaluating);
 		} else {
 			humanGHeuristic -= heuristic(evaluating);
@@ -238,6 +237,6 @@ public class Board extends JFrame implements ActionListener {
 		gHeuristic = computerGHeuristic - humanGHeuristic;
 		g.removeVertex(evaluating);
 		evaluating.setPiece(Piece.EMPTY);
-		
+
 	}
 }
